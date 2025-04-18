9bd0b4940237f75be6d5757addf513c3eec639a1
tomas-vergara <tomasvergara272@gmail.com>
2025-04-17 21:22:28 -0300

    gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    SERVICE_ID: 'bim_fellow_id',
+    NOTIFICATION_TEMPLATE_ID: 'template_8h8jhox',
+    RESPONSE_TEMPLATE_ID: 'template_z7lr2e3',
+    MAIL_BIMFELLOW: 'postctester2@gmail.com',
+    PUBLIC_KEY: 'PUBLIC_KEY',
+};
