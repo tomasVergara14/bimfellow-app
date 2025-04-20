cbd975701cc785c76e57dd555de8bc47269bb4e9
tomas-vergara <tomasvergara272@gmail.com>
2025-04-19 23:29:04 -0300

    package

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+
+export const environment = {
+  production: true,
+  SERVICE_ID: 'your_service_id',
+  NOTIFICATION_TEMPLATE_ID: 'your_notification_template_id',
+  RESPONSE_TEMPLATE_ID: 'your_response_template_id',
+  MAIL_BIMFELLOW: 'your_email',
+  PUBLIC_KEY: 'your_public_key',
+};
