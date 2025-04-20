ca00a379cd709bf83c26b6839ee8e048602fdd33
tomas-vergara <tomasvergara272@gmail.com>
2025-04-19 22:28:43 -0300

    netlify

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,16 +1,19 @@
-import fs from 'fs';
-import path from 'path';
+// scripts/create-env.ts o .js (como prefieras)
+const fs = require('fs');
+const path = require('path');
 
-const content = `
+const targetPath = path.join(__dirname, '../src/environments/environment.prod.ts');
+
+const envFileContent = `
 export const environment = {
   production: true,
-  EMAIL_JS_ID: "${process.env.EMAIL_JS_ID}",
-  SERVICE_ID: "${process.env.SERVICE_ID}",
-  NOTIFICATION_TEMPLATE_ID: "${process.env.NOTIFICATION_TEMPLATE_ID}",
-  RESPONSE_TEMPLATE_ID: "${process.env.RESPONSE_TEMPLATE_ID}",
-  MAIL_BIMFELLOW: "${process.env.MAIL_BIMFELLOW}",
-  PUBLIC_KEY: "${process.env.PUBLIC_KEY}"
+  SERVICE_ID: '${process.env.SERVICE_ID}',
+  NOTIFICATION_TEMPLATE_ID: '${process.env.NOTIFICATION_TEMPLATE_ID}',
+  RESPONSE_TEMPLATE_ID: '${process.env.RESPONSE_TEMPLATE_ID}',
+  MAIL_BIMFELLOW: '${process.env.MAIL_BIMFELLOW}',
+  PUBLIC_KEY: '${process.env.PUBLIC_KEY}',
 };
 `;
 
-fs.writeFileSync(path.join('src/environments/environment.prod.ts'), content.trim());
+fs.writeFileSync(targetPath, envFileContent);
+console.log(`✔️ Environment file created at ${targetPath}`);
